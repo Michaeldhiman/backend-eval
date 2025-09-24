cb98d5ff3234b69ed39f636d93b576a304f4397d
Michaeldhiman <michaeldhiman0214@gmail.com>
2025-09-24 12:05:26 +0530

    correct the middleware

--- backend/src/middlewares/multer.middlewares.js
+++ backend/src/middlewares/multer.middlewares.js
@@ -1,32 +1,13 @@
+import multer from "multer";
 
-import jwt from "jsonwebtoken";
-import { config } from "../config/index.js";
-import { ApiError } from "../utils/ApiError.js";
-import { User } from "../models/user.models.js";
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/temp')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname)
+  }
+});
 
-export const isLoggedIn = async(req, _, next)=>{
-    // if user have cookies data then get the data
-    // if data is wrong throw error
-    // return req.user
-    const token = req.cookies.token || req.headers['token']?.replace("Barrer ", "");
-    // console.log(token);
-    try {
-        if(!token){
-            throw new ApiError(401, "User must be logged in");
-        }
-
-        const decoded = jwt.verify(token, config.accessTokenSecret);
-
-        if(!decoded){
-            throw new ApiError(401, "User token is invalid");
-        }
-
-        const user = await User.findById(decoded?._id).select("-password");
-
-        req.user = user;
-
-    } catch (error) {
-        throw error;
-    }
-    next();
-}
+export const upload = multer({ storage: storage });
